86ba0527fe3ab9df492e130e374d4a2d30a3526c
enikiforov <likechokolit@gmail.com>
2018-06-29 17:15:13 +0300

    Password types #51

--- scripts/init.js
+++ scripts/init.js
@@ -2,10 +2,30 @@ if(!window.CONFIG) {
    alert('Your config is incorrect. Please make sure "config.js" is valid javascript');
 }
 
-var api = new Api(CONFIG.wsUrl, CONFIG.password);
+var apiPassword;
+
+if(!CONFIG.passwordType || CONFIG.passwordType === PASSWORD_TYPES.MANUAL) {
+   apiPassword = CONFIG.password;
+}
+else if(CONFIG.passwordType === PASSWORD_TYPES.PROMPT) {
+   apiPassword = askPassword(false);
+}
+else if(CONFIG.passwordType === PASSWORD_TYPES.PROMPT_SAVING) {
+   apiPassword = askPassword(true);
+}
+
+console.log('apiPassword', apiPassword);
+
+var api = new Api(CONFIG.wsUrl, apiPassword);
 
 var App = angular.module('App', ['pr.longpress']);
 
 App.config(function($sceProvider) {
    $sceProvider.enabled(false);
 });
+
+if(CONFIG.passwordType === PASSWORD_TYPES.PROMPT_SAVING) {
+   api.onReady(function () {
+      savePassword(apiPassword);
+   });
+}
