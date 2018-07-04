e5651cb85e19712851995fc2f6beafb538eda97c
Alexey <me@reso.od.ua>
2018-07-04 17:47:10 +0100

    Added "time ago" helper function

--- scripts/app.js
+++ scripts/app.js
@@ -121,6 +121,59 @@ function playSound(sound) {
    audio.play();
 }
 
+function timeAgo (time) {
+   time = +new Date(time);
+
+   var timeFormats = [
+      [60, 'seconds', 1],
+      [120, '1 minute ago', '1 minute from now'],
+      [3600, 'minutes', 60],
+      [7200, '1 hour ago', '1 hour from now'],
+      [86400, 'hours', 3600],
+      [172800, 'a day ago', 'Tomorrow'],
+      [604800, 'days', 86400],
+      [1209600, 'Last week', 'Next week'],
+      [2419200, 'weeks', 604800],
+      [4838400, 'a month ago', 'Next month'],
+      [29030400, 'months', 2419200],
+      [58060800, 'a year ago', 'Next year'],
+      [2903040000, 'years', 29030400],
+      [5806080000, 'Last century', 'Next century'],
+      [58060800000, 'centuries', 2903040000]
+   ];
+
+   var seconds = (+new Date() - time) / 1000,
+      token = 'ago',
+      list_choice = 1;
+
+   if (seconds < 10) {
+      return 'just now'
+   }
+
+   if (seconds < 60) {
+      return 'less than a minute ago'
+   }
+
+   if (seconds < 0) {
+      seconds = Math.abs(seconds);
+      token = 'from now';
+      list_choice = 2;
+   }
+
+   var i = 0, format;
+
+   while (format = timeFormats[i++]) {
+      if (seconds < format[0]) {
+         if (typeof format[2] == 'string')
+            return format[list_choice];
+         else
+            return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+      }
+   }
+
+   return time;
+}
+
 function debounce(func, wait, immediate) {
    var timeout;
    return function() {
