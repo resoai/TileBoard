b5f6a09c52b04fa5cb5479f525847b8741122599
ak-test <akloeckner@users.noreply.github.com>
2019-10-26 23:36:12 +0200

    Allow override of `item.action` and `item.longClickAction`
    This caters for ideas as described in #274. If we can always override the `item`'s `action` or `longCLickAction` we can make more fancy things. And TileBoard becomes actually more "customizable".

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -36,6 +36,10 @@ function MainController ($scope, $location) {
    var popupIframeStyles = {};
 
    $scope.entityClick = function (page, item, entity) {
+      if(typeof item.action === "function") {
+         return callFunction(item.action, [item, entity]);
+      }
+
       switch (item.type) {
          case TYPES.SWITCH:
          case TYPES.LIGHT:
@@ -62,7 +66,6 @@ function MainController ($scope, $location) {
 
          case TYPES.ALARM: return $scope.openAlarm(item, entity);
 
-         case TYPES.CUSTOM: return $scope.customTileAction(item, entity);
          case TYPES.DIMMER_SWITCH: return $scope.dimmerToggle(item, entity);
 
          case TYPES.POPUP_IFRAME: return $scope.openPopupIframe(item, entity);
@@ -72,6 +75,10 @@ function MainController ($scope, $location) {
    };
 
    $scope.entityLongClick = function ($event, page, item, entity) {
+      if(typeof item.longClickAction === "function") {
+         return callFunction(item.longClickAction, [item, entity]);
+      }
+
       switch (item.type) {
          case TYPES.LIGHT: return $scope.openLightSliders(item, entity);
       }
@@ -1064,12 +1071,6 @@ function MainController ($scope, $location) {
             entity_id: item.id
          }
       });
-   };
-
-   $scope.customTileAction = function (item, entity) {
-      if(item.action && typeof item.action === "function") {
-         callFunction(item.action, [item, entity]);
-      }
    };
 
    $scope.sendPlayer = function (service, item, entity) {
