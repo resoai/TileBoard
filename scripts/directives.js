1f973588136a1b7b4da8c11461fe29ff0910431a
Alexey <me@reso.od.ua>
2019-12-17 16:47:00 +0000

    Mute stream video

--- scripts/directives.js
+++ scripts/directives.js
@@ -186,6 +186,7 @@ App.directive('cameraStream', ['Api', function (Api) {
          var appendVideo = function (url) {
             var el = document.createElement('video');
             el.style.width = '100%';
+            el.muted = 'muted';
 
             var hls = new Hls();
             hls.loadSource(url);
