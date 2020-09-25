c476c006cb062df10654952faa39caf33975fe30
Rafał Chłodnicki <rchl2k@gmail.com>
2020-09-25 22:15:52 +0200

    Fix references to assets in CSS and generate hashed bundles in production

--- rollup.config.js
+++ rollup.config.js
@@ -5,21 +5,27 @@ import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-cpy';
 import del from 'rollup-plugin-delete';
 import emitEJS from 'rollup-plugin-emit-ejs';
-import less from 'rollup-plugin-less';
 import resolve from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
+import styles from 'rollup-plugin-styles';
 import { terser } from 'rollup-plugin-terser';
 
 const isProduction = process.env.PRODUCTION === 'true';
 let outDir = '';
+let outputJsName = ''
+let outputCssName = ''
 const appPlugins = [];
 
 if (isProduction) {
    outDir = 'dist';
+   outputJsName = 'app-[hash].js'
+   outputCssName = 'styles-[hash][extname]'
    appPlugins.push(terser());
 }
 else {
    outDir = 'dist-dev';
+   outputJsName = 'app.js'
+   outputCssName = 'styles[extname]'
    appPlugins.push(serve({
       contentBase: outDir,
       port: 8080,
@@ -30,8 +36,10 @@ else {
 const config = {
    input: './scripts/index.js',
    output: {
+      // Defines the output path of the extracted CSS.
+      assetFileNames: `styles/${outputCssName}`,
       dir: outDir,
-      entryFileNames: 'scripts/app.js',
+      entryFileNames: `scripts/${outputJsName}`,
       format: 'iife',
       globals: {
          '@babel/runtime/regenerator': 'regeneratorRuntime',
@@ -42,7 +50,11 @@ const config = {
    plugins: [
       // Clean up output directory before building.
       del({
-         targets: [`${outDir}/scripts/app.*`],
+         targets: [
+            `${outDir}/assets/`,
+            `${outDir}/scripts/app*`,
+            `${outDir}/styles/styles*`,
+         ],
       }),
       progress(),
       commonjs(),
@@ -55,8 +67,17 @@ const config = {
             'angularjs-annotate',
          ],
       }),
-      less({
-         output: `${outDir}/styles/styles.css`,
+      styles({
+         // Extract CSS into separate file (path specified through output.assetFileNames).
+         mode: 'extract',
+         // Don't try to resolve CSS @imports.
+         import: false,
+         sourceMap: true,
+         url: {
+            hash: 'assets/[name]-[hash][extname]',
+            // The public path where assets referenced from css files are available.
+            publicPath: '../assets/'
+         },
       }),
       emitEJS({
          src: '.',
@@ -68,6 +89,7 @@ const config = {
          { files: './favicon.png', dest: `./${outDir}/` },
          { files: './manifest.webmanifest', dest: `./${outDir}/` },
          { files: './images/*.*', dest: `./${outDir}/images/` },
+         // Copy over empty custom.css but don't overwrite in case user has customized it.
          { files: './styles/custom.css', dest: `./${outDir}/styles/`, options: { overwrite: false } },
       ]),
       ...appPlugins,
