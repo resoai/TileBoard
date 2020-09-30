525cee00cb7fc94b8ea1180cd02626a38eacf6fc
ak-test <akloeckner@users.noreply.github.com>
2020-09-30 22:15:32 +0100

    eslint

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1759,14 +1759,14 @@ App.controller('Main', function ($scope, $timeout, $location, Api) {
    };
 
    $scope.openDoorEntry = function (item, entity) {
-      return initPopupLayout(item, entity, "_doorEntry",
+      return initPopupLayout(item, entity, '_doorEntry',
          angular.merge({
             classes: ['-popup-fullsize'],
             styles: {},
             items: [angular.merge({
                state: false,
                title: false,
-               position: [0,0],
+               position: [0, 0],
                classes: ['-tile-fullsize'],
                customStyles: {
                   width: null,
@@ -1775,7 +1775,7 @@ App.controller('Main', function ($scope, $timeout, $location, Api) {
                   left: null,
                },
             }, getItemFieldValue('layout.camera', item, entity))]
-            .concat(getItemFieldValue('layout.tiles', item, entity) || [])
+               .concat(getItemFieldValue('layout.tiles', item, entity) || []),
          }, getItemFieldValue('layout.page', item, entity)));
    };
 
