fd41c125491072089d783cb522f9e729063737f6
Rafał Chłodnicki <rchl2k@gmail.com>
2021-03-11 14:52:52 +0100

    Examples that don't require real entities

--- config.example.js
+++ config.example.js
@@ -360,18 +360,23 @@ var CONFIG = {
                         {
                            title: 'Free Memory',
                            icon: 'mdi-memory',
-                           value: function() {return this.$scope.filterNumber(this.parseFieldValue('&sensor.memory_free.state',null,null),1) + ' GB';}
+                           value: function() {
+                              // var freeMemory = this.parseFieldValue('&sensor.memory_free.state')
+                              var freeMemory = 15.444  // Just an example.
+                              return this.$scope.filterNumber(freeMemory, 1) + ' GB';
+                           }
                         },
                      ]
-                  },                  
+                  },
                   {
                      position: [0, 1.5],
                      width: 1.5,
-                     height: 1,
+                     height: 1.5,
                      title: 'My Gauge Title',
                      subtitle: '',
                      type: TYPES.GAUGE,
-                     id: 'sensor.my_sample_sensor', // Assign the sensor you want to display on the gauge
+                     // id: 'sensor.my_sample_sensor', // Assign the sensor you want to display on the gauge
+                     id: {state: 11111}, // Remove after choosing to actual sensor ID
                      value: function(item, entity) {
                         return entity.state;
                      },
